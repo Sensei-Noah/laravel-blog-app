0975e22e3b4a697c246e4967fc515e595d30bab4
WorkoutKing <kl4650@gmail.com>
2022-05-10 09:44:52 +0300

    a lot of stuff

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,12 @@ use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
+
+    public function __construct(){
+
+        $this->middleware('auth', ['except'=>['index', 'showPost']]);
+    }
+
     public function index(){
         $posts = Post::paginate(1);
         return view('pages.home', compact('posts'));
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,8 @@ Route::get('/post/update/{post}', [PostController::class, 'editPost']);
 Route::get('/post/delete/{post}', [PostController::class, 'deletePost']);
 Route::post('/store', [PostController::class, 'store']);
 Route::post('/update/{post}', [PostController::class, 'storeUpdate']);
+
+Auth::routes();
+
+Route::get('/logout', 'App\Http\Controllers\Auth\LoginController@logout');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
